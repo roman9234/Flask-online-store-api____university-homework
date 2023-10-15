86693a93fb39fd7593b637ec577965d8370c323c
roman9234 <roman_lesovoy@mail.ru>
2023-10-15 20:02:41 +0300

    первые 2 функции для работы с БД. Пока только как образцы кода. Нужно будет как-то добавить в git саму БД, чтобы можно было быстро развернуть её на любом копьютере и работать с бэком

---
+++ config.py
@@ -0,0 +1,7 @@
+host_c = "127.0.0.1"
+user_c = "postgres"
+password_c = "" #пока так, это просто пример кода который робит только у меня на компе. Инфа как перекачать базу данных в git вроде есть в agile проекте
+# password_c = "rootroot"
+db_name_c = "simple_online_store_db"
+port_c = 5432
+token = ""
