497412d642240908ce6441c6e9cebaed4dd083c8
roman9234 <roman_lesovoy@mail.ru>
2023-10-22 16:09:22 +0300

    удалён hardcoded secret

--- online_store/authorization_module/settings.py
+++ online_store/authorization_module/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'simple_online_store_db',
         'USER': 'postgres',
-        'PASSWORD': '164014',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
